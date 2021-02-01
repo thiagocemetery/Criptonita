ae9ad4f75265d063d30e9ebf6d999abd03d8c2ce
bittencourthiago <thiago.bittc@gmail.com>
2021-02-01 11:22:40 -0300

    Teste do coin model

--- Criptonita/CriptonitaTests/CriptonitaTests.swift
+++ Criptonita/CriptonitaTests/CriptonitaTests.swift
@@ -10,34 +10,33 @@ import XCTest
 
 class CriptonitaTests: XCTestCase {
 
-    let homeViewModel:HomeViewModel = HomeViewModel()
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-//    func filtraCelulasMoedas(usertext: String) {
-//        if usertext == "" {
-//            arrayFiltrados = celulasMoedas
-//        } else {
-//            let filtradas = celulasMoedas.filter {
-//                guard let searchCoin = $0.labelNome.text?.lowercased().contains(usertext.lowercased()) else { return false}
-//                return searchCoin
-//            }
-//            arrayFiltrados = filtradas
-//        }
-//    }
-    func testVerificaSeBuscaEstaRetornandoArrayCerto() {
-        let busca = "Bit"
-        
-        homeViewModel.filtraCelulasMoedas(usertext: busca)
-        
-        for moeda in homeViewModel.arrayFiltrados {
-            
-            XCTAssertEqual(true, moeda.labelNome.text?.contains(busca))
-            
-        }
-        
+    func testModel() {
+
+        let coin = ModelCoinElement(assetID: "BTC", name: "Bitcoin", type_is_crypto: 1, volume1HrsUsd: 0.0, volume1DayUsd: 0.0, volume1MthUsd: 0.0, priceUsd: 0.0, idIcon: "imagem.png")
+
+        XCTAssertEqual(coin.assetID, "BTC")
+
+        XCTAssertEqual(coin.name, "Bitcoin")
+
+        XCTAssertEqual(coin.type_is_crypto, 1)
+
+        XCTAssertEqual(coin.volume1HrsUsd, 0.0)
+
+        XCTAssertEqual(coin.volume1DayUsd, 0.0)
+
+        XCTAssertEqual(coin.volume1MthUsd, 0.0)
+
+        XCTAssertEqual(coin.priceUsd, 0.0)
+
+        XCTAssertEqual(coin.idIcon, "imagem.png")
+
+        XCTAssertNotNil(coin)
+
     }
 
 }
